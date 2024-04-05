e5c5bdcddab52c01db8cce5c8bff1bc0690cf047
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2024-04-05 16:10:43 -0300

    ex 04

---
+++ pratica03/ex04.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+  int numUser = 5;
+  int fat = 0;
+  int cont = 0;
+
+  // printf("Digite um numero Para saber Seu fatorial: ");
+  // scanf("%d",&numUser);
+
+
+  for (cont ; cont <= numUser; cont++)
+  {
+    
+  }
+  
+
+
+  return 0;
+}
